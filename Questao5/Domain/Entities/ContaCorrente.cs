38e5e37dbcde9bd592d17a693b1a0419e37e412d
Mauricio-HNS <90620341+Mauricio-HNS@users.noreply.github.com>
2023-06-22 20:14:59 -0300

    Commit-09
    Correção na classe ContaCorrente identificadores.

--- Questao5/Domain/Entities/ContaCorrente.cs
+++ Questao5/Domain/Entities/ContaCorrente.cs
@@ -5,6 +5,8 @@ namespace Questao5.Domain.Entities
     public class ContaCorrente
     {
         public int Id { get; set; }
+        public string Numero { get; set; }
+        public string Nome { get; set; }
         public decimal Saldo { get; set; }
         public List<Movimento> Movimentos { get; set; }
         public bool Ativo { get; internal set; }
