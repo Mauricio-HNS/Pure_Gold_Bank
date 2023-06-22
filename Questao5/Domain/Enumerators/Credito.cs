690c39742a4071d0f7e084a9dbf181de60e20ace
Mauricio-HNS <90620341+Mauricio-HNS@users.noreply.github.com>
2023-06-22 20:28:39 -0300

    Commit-11
    Debito e Credito implementados.

--- Questao5/Domain/Enumerators/Credito.cs
+++ Questao5/Domain/Enumerators/Credito.cs
@@ -1,6 +1,28 @@
-﻿namespace Questao5.Domain.Enumerators
+﻿using Questao5.Infrastructure.Services;
+using System;
+
+namespace Questao5.Domain
 {
     public class Credito
     {
+        public Guid Id { get; set; }
+        public Guid IdContaCorrente { get; set; }
+        public DateTime DataMovimento { get; set; }
+        public TipoMovimento TipoMovimento { get; set; }
+        public decimal Valor { get; set; }
+
+        public Credito(Guid id, Guid idContaCorrente, DateTime dataMovimento, TipoMovimento tipoMovimento, decimal valor)
+        {
+            Id = id;
+            IdContaCorrente = idContaCorrente;
+            DataMovimento = dataMovimento;
+            TipoMovimento = tipoMovimento;
+            Valor = valor;
+        }
+
+        public void MovimentarContaCorrente()
+        {
+            // TODO: Implementar lógica para movimentar a conta corrente
+        }
     }
 }
