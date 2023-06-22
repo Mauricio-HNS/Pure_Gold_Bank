cc6462e6d3798fa34b412e36c5281ba31350174b
Mauricio-HNS <90620341+Mauricio-HNS@users.noreply.github.com>
2023-06-22 17:22:18 -0300

    Commit - 01
    Implantacao do Projeto.
    Questão 1 Respondida

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public string NomeTitular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial = 0.0)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            Saldo = depositoInicial;
+        }
+
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            if (Saldo < quantia)
+            {
+                Console.WriteLine("Saldo insuficiente!");
+            }
+            else
+            {
+                Saldo -= quantia + 3.5;
+                Console.WriteLine("Saque realizado com sucesso!");
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + NumeroConta
+                + ", Titular: "
+                + NomeTitular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
